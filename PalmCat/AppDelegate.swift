2ad3a7fd908926598074fdf62f98259b84cb3df5
junsungpark <jimmy@junsoft.org>
2020-10-28 15:44:07 +0900

    initial

---
+++ PalmCat/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PalmCat
+//
+//  Created by Junsung Park on 2020/10/28.
+//  Copyright © 2020 Junsung Park. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PalmCat/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PalmCat
+//
+//  Created by Junsung Park on 2020/10/28.
+//  Copyright © 2020 Junsung Park. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
